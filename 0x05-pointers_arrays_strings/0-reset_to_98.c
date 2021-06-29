5759959065699ee6349bd189e7dc5c1cbe1eb060
princesszion <maikemvictorine@gmail.com>
2021-06-29 11:39:15 -0700

    modified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
 /**
-*n is an interter and int is also an interger
-*/
+ *reset_to_98 - reasign value in address memory
+ *@n: parameter enteger
+ *Return: void
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+  *n = 98;
 }
 /**
 *This code changes the interger to 98
