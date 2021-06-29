b8550accda1eef2db8525316e8e893325744eb9b
princesszion <maikemvictorine@gmail.com>
2021-06-29 13:09:20 -0700

    put a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* _puts - Prints a string to stdout.
+* @str: The string to be printed.
+*/
+void _puts(char *str)
+{
+while (*str)
+putchar(*str++);
+putchar('\n');
+}
