9aa231751e6ae38f95aa0d2c62defd35bbcfba32
princesszion <maikemvictorine@gmail.com>
2021-07-03 07:00:43 -0700

    uses the put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - prints "Programming is like building a multilingual puzzle"
+*
+*Return: 0 on sucess
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
