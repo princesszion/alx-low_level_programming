bd1540f2b6a253c7c5951a7a1aa0d084f0fd8dd5
princesszion <maikemvictorine@gmail.com>
2021-07-17 09:18:39 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*print_alphabet_x10 - prints alphabets x10
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 1; i < 11; i++)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+}
