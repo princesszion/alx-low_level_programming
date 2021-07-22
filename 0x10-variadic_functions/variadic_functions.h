d2391a4cf919c226f284b35dafc7d24a5207d1cd
princesszion <maikemvictorine@gmail.com>
2021-07-22 07:23:03 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 /**
 *formate - Typedef struct formats
 *struct formats - format to type print
