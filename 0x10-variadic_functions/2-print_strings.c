84e0d701db63f2431ec8e6cb2c55f3777f7529f4
princesszion <maikemvictorine@gmail.com>
2021-07-22 08:06:03 -0700

    print charaters

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_strings - prints strings, followed by a new line.
+*@separator: string to separator
+*@n: size
+*Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list valist;
+unsigned int i;
+char *ptr;
+va_start(valist, n);
+for (i = 0; i < n; i++)
+{
+ptr = va_arg(valist, char *);
+if (ptr)
+printf("%s", ptr);
+else
+printf("(nil)");
+if (i < (n - 1) &&  separator)
+printf("%s", separator);
+}
+printf("\n");
+va_end(valist);
+}
