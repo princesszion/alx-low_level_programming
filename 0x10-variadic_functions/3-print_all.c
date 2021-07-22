4903938c956b05871ac60eb1189f1832c77bcc10
princesszion <maikemvictorine@gmail.com>
2021-07-22 12:54:12 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,75 +6,38 @@
 *@valist: is a arg type char.
 *Return: void
 */
-void func_char(va_list valist)
-{
-printf("%c", (char)va_arg(valist, int));
-}
-/**
-*func_int - print type int.
-*@valist: is a arg type int.
-*Return: void
-*/
-void func_int(va_list valist)
-{
-printf("%i", va_arg(valist, int));
-}
-/**
-*func_float - print type float.
-*@valist: is a arg type float.
-*Return: void
-*/
-void func_float(va_list valist)
-{
-printf("%f", (float)va_arg(valist, double));
-}
-/**
-*func_string - print type string.
-*@valist: is a arg type string
-*Return: void
-*/
-void func_string(va_list valist)
-{
-char *ptr = va_arg(valist, char *);
-if (ptr == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", ptr);
-}
-/**
-*print_all - prints anything
-*@format: is a list of types of arguments passed to the function
-*Return: void
-*/
 void print_all(const char * const format, ...)
 {
-va_list valist;
 int i = 0, j = 0;
-char *sp = "";
-formate formates[] = {
-{"c", func_char},
-{"i", func_int},
-{"f", func_float},
-{"s", func_string},
-{NULL, NULL}};
-va_start(valist, format);
+char *res;
+if (!format)
+return;
+va_list stuff;
+va_start(stuff, format);
 while (format && format[i])
-    {
-j = 0;
-while (formates[j].index)
 {
-if (*formates[j].index == format[i])
+switch (format[i++])
 {
-printf("%s", sp);
-formates[j].func(valist);
-sp = ", ";
+case 'c':
+printf("%c", va_arg(stuff, int));
+break;
+case 'i':
+printf("%d", va_arg(stuff, int));
+break;
+case 'f':
+printf("%f", (float)va_arg(stuff, double));
+break;
+case 's':
+res = va_arg(stuff, char *);
+if (res == NULL)
+res = "(nil)";
+printf("%s", res);
+break;
+default:
+continue;
 }
-j++;
-}
-i++;
+if (format[i])
+printf(", ");
 }
 printf("\n");
-va_end(valist);
 }
