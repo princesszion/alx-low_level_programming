5fa3aae1dfbe438d9f5724988709a28c0a7ba043
princesszion <maikemvictorine@gmail.com>
2021-07-22 05:54:10 -0700

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+*formate - Typedef struct formats
+*struct formats - format to type print
+*@index: index to type
+*@func: pointer to function - function print
+*/
+typedef struct formats
+{
+char *index;
+void (*func)(va_list valist);
+} formate;
+#endif /* VARIADIC_FUNCTIONS_H */
