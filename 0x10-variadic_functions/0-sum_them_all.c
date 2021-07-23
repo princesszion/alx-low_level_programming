a0cc56d08004b78208730e7e69e4f4af7729043b
princesszion <maikemvictorine@gmail.com>
2021-07-23 01:26:09 -0700

    sum all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,9 +2,9 @@
 #include "variadic_functions.h"
 /**
 * sum_them_all - sums of all its parameters.
-* @n: number of parameters
+* @n:number of parameters
 *
-* Return: sum of all parameters
+* Return:sum of all parameters
 */
 int sum_them_all(const unsigned int n, ...)
 {
