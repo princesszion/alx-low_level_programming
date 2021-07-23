f77e04398aeb84d0fe67673cca38fd3be701bb17
princesszion <maikemvictorine@gmail.com>
2021-07-23 12:27:44 -0700

    compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
 *
 * Return: outputs the difference of s1 and s2
 */
-int _strcmp(char * s1, char * hs2)
+int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
