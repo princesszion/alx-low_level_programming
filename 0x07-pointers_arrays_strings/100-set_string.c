7c1ea1189f543044ad9dbdf21389c448f03cc5fc
princesszion <maikemvictorine@gmail.com>
2021-07-27 09:11:30 -0700

    changed

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 /**
 * set_string - sets the value of a pointer to a char
-* @s: pointer to change
-* @to: string to change pointer to
-*
-* Return: void
+* @s: content is to be copied
+*@to: string
+*Return: void
 */
 void set_string(char **s, char *to)
 {
