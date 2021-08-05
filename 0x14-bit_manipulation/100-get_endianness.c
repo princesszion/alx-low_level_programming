f305c1a72c1afc0aecccd2cbad75f05a0db8e0f3
princesszion <maikemvictorine@gmail.com>
2021-08-05 15:46:04 -0700

    changed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-* get_endianness - checks if computer is big or little edian
-* Return: 0 if big edian and 1 if little edian
+* get_endianness - Checks the endianness.
+*
+* Return: If big-endian - 0.
+*         If little-endian - 1.
 */
 int get_endianness(void)
 {
-char num;
-char *ptr;
-num = '1';
-ptr = &num;
-if (*ptr == 0)
-return (0);
-else
+int num = 1;
+char *endian = (char *)&num;
+if (*endian == 1)
 return (1);
+return (0);
 }
