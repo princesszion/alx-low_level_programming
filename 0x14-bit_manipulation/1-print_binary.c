8070fd1fa952ac5e94a80874252114785956170c
princesszion <maikemvictorine@gmail.com>
2021-08-05 15:19:23 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,44 @@
-#include "holberton.h"
+#include "main.h"
 /**
-* rev_bin - prints the binary
-* @n: number in decimal
-* Return: void
+* _pow - calculates (base ^ power)
+* @base: base of the exponent
+* @power: power of the exponent
+*
+* Return: value of (base ^ power)
 */
-void rev_bin(unsigned long int n)
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
-if (!n)
-return;
-rev_bin(n >> 1);
-_putchar((n & 1) + '0');
+unsigned long int num;
+unsigned int i;
+num = 1;
+for (i = 1; i <= power; i++)
+num *= base;
+return (num);
 }
 /**
-* print_binary - prints the binary representation of a number.
-* @n: number in decimal
+* print_binary - prints a number in binary notation
+* @n: number to print
+*
 * Return: void
 */
 void print_binary(unsigned long int n)
 {
-if (!n)
+unsigned long int divisor, check;
+char flag;
+flag = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+check = n & divisor;
+if (check == divisor)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || divisor == 1)
+{
 _putchar('0');
-else
-rev_bin(n);
+}
+divisor >>= 1;
+}
 }
